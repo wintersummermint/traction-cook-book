f7de4a561f8f1291d6855bcf83ce9a2ababce49d
Nixon <galarionixon@gmail.com>
2017-07-31 15:15:56 +0800

    initial add new page with unfinished forms

--- src/components/addnew.jsx
+++ src/components/addnew.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { Row, Col, Input, Button } from 'react-materialize';
+import $ from 'jquery';
 
 class AddNew extends Component {
+
 	render() {
 		console.log('recipes :', this.props.recipes);
 		return (
-			<div><h1>ijdiwoqjdoiwqjf</h1><h1>ijdiwoqjdoiwqjf</h1><h1>ijdiwoqjdoiwqjf</h1><h1>ijdiwoqjdoiwqjf</h1><h1>ijdiwoqjdoiwqjf</h1><h1>ijdiwoqjdoiwqjf</h1></div>
+			<div className="container">
+				<div className="center-align m-t-20">
+					<h4 className="my-pink">Add Recipe</h4>
+				</div>
+				
+				<Row className="m-t-40">
+					<Col m={12} s={12}>
+						<Input s={12} placeholder="Recipe Name" validate />
+					</Col>
+					<Col m={12} s={12}>
+						<div className="input-field col s12">
+				          <textarea placeholder="Description" className="materialize-textarea" rows="6"></textarea>
+				        </div>
+					</Col>
+				</Row>
+
+				<Row>
+					<Col m={12} s={12}>
+						<div className="p-l-15 p-r-15">
+							<h5 className="semi-black"><small>Ingredients</small></h5>
+
+
+						</div>
+					</Col>
+					<Col m={12} s={12}>
+						<Input s={10} placeholder="Recipe Name" validate />
+						<Button className="btn-block inline col s2 m-t-10">Add</Button>
+					</Col>
+				</Row>
+			</div>
 		);
 	}
 }
